3d97338d8822178512ecd238abfb25e823d4a0f1
Ricardo Moreira <ricardo.d.moreira.rm@gmail.com>
2019-04-24 23:01:33 +0300

    continue with the tour page

--- pages/tour/index.vue
+++ pages/tour/index.vue
@@ -1,36 +1,32 @@
 <template>
   <section class="container">
-    <nuxt-link v-for="tour in tours" :key="tour.id" :to="tour.id">
-      <article class="tour-preview">
-        <div class="tour-preview-content">
-          <h1>{{ tour.country }}</h1>
-          <p>{{ tour.city }}</p>
-          <p>{{ tour.date }}</p>
-        </div>
-      </article>
-    </nuxt-link>
+    <Tour
+      v-for="tour in tours"
+      :key="tour.id"
+      :country="tour.title"
+      :city="tour.city"
+      :date="tour.date"
+      :id="tour.id"
+    />
   </section>
 </template>
 
 <script>
+import Tour from '@/components/Tour/Tour'
 export default {
-  data() {
-    return {
-      tours: [
-        {
-          country: 'Finland',
-          city: 'Helsinki',
-          date: '01/02/2020',
-          id: 'Finland'
-        },
-        {
-          country: 'Portugal',
-          city: 'Lisbon',
-          date: '01/05/2021',
-          id: 'Portugal'
-        }
-      ]
-    }
+  components: {
+    Tour
+  },
+  asyncData(context) {
+    return context.app.$storyapi
+      .get('cdn/stories', {
+        version: 'draft',
+        starts_with: 'tour/'
+      })
+      .then(res => {
+        console.log(res)
+        return res
+      })
   }
 }
 </script>
