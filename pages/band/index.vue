00580dd9984a14dc737845900c13e10f825d1d21
Ricardo Moreira <ricardo.d.moreira.rm@gmail.com>
2019-05-14 22:53:31 +0300

    finishing band page

--- pages/band/index.vue
+++ pages/band/index.vue
@@ -1,40 +1,57 @@
 <template>
-  <div class="container">
-    <div class="make"></div>
-    <div class="jape"></div>
-    <div class="jussi"></div>
-  </div>
+  <section>
+    <div class="container">
+      <div class="make"></div>
+      <div class="jape"></div>
+      <div class="jussi"></div>
+    </div>
+  </section>
 </template>
 
 
 <style scoped>
 html,
 body {
-  height: 100%;
   margin: 0;
 }
 .container {
   display: grid;
-  min-height: 100%;
+
+  border-radius: 3px;
+  width: 90%;
+  margin: auto;
+  height: 20rem;
+  margin: 1rem;
   grid-template-columns: 1fr 1fr 1fr;
   grid-template-rows: 1fr;
   grid-template-areas: 'make jape jussi';
+  column-gap: 2rem;
 }
 
 .make {
   grid-area: make;
-  min-height: 100%;
-  background: url(https://images.unsplash.com/photo-1557753478-25c7d9f05bf5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  height: 20rem;
+  background-image: url(https://cdn1.imggmi.com/uploads/2019/5/14/a10f2a4f63155e3b191188f02bd983ae-full.jpg);
 }
 
 .jape {
   grid-area: jape;
-  min-height: 100%;
-  background: red;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  height: 20rem;
+  background-image: url(https://cdn1.imggmi.com/uploads/2019/5/14/c00fa49b9268e04334d21e586a37889d-full.jpg);
 }
 
 .jussi {
   grid-area: jussi;
-  min-height: 100%;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  height: 20rem;
+  background-image: url(https://cdn1.imggmi.com/uploads/2019/5/14/b992742fda41747d60758e70ef36e60a-full.jpg);
 }
 </style>
